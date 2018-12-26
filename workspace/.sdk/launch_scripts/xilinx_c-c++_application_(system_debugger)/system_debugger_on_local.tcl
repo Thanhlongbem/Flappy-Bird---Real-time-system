dcde0fe7561b98d8ed54602aebaa5e622c57c6cb
DESKTOP-9QIT1K7\Fptshop <thanhlongny@gmail.com>
2018-12-26 09:02:04 +0700

    update

---
+++ workspace/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_on_local.tcl
@@ -0,0 +1,13 @@
+connect -url tcp:127.0.0.1:3121
+targets -set -nocase -filter {name =~ "microblaze*#1" && bscan=="USER2"  && jtag_cable_name =~ "Digilent JTAG-SMT1 210203A3D06DA"} -index 0
+loadhw D:/HeThongNhungThoiGianThuc/workspace/system_top_hw_platform_1/system.hdf
+targets -set -nocase -filter {name =~ "microblaze*#1" && bscan=="USER2"  && jtag_cable_name =~ "Digilent JTAG-SMT1 210203A3D06DA"} -index 0
+rst -processor
+targets -set -nocase -filter {name =~ "microblaze*#1" && bscan=="USER2"  && jtag_cable_name =~ "Digilent JTAG-SMT1 210203A3D06DA"} -index 0
+dow D:/HeThongNhungThoiGianThuc/workspace/driver_HDMI/Debug/driver_HDMI.elf
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent JTAG-SMT1 210203A3D06DA"} -index 0
+dow D:/HeThongNhungThoiGianThuc/workspace/MB1_RTOS/Debug/MB1_RTOS.elf
+targets -set -nocase -filter {name =~ "microblaze*#1" && bscan=="USER2"  && jtag_cable_name =~ "Digilent JTAG-SMT1 210203A3D06DA"} -index 0
+con
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent JTAG-SMT1 210203A3D06DA"} -index 0
+con
